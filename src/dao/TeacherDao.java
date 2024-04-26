90ef4eee59f62c24a9a1dd7446d658f6cf1ff14b
maemura589 <knz2374523@stu.o-hara.ac.jp>
2024-04-26 14:55:39 +0900

    login表示まで完了

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -1,10 +1,37 @@
 //package dao;
 
-//import bean.Maemura;
+//import bean.Maemura
 //import java.sql.Connection;
 //import java.sql.PreparedStatement;
 //import java.sql.ResultSet;
+//import java.util.ArrayList;
+//import java.util.List;
 
-//public class TeacherDao extends Dao {
-	//public Maemura search
+//public class TeacherDAO extends Dao {
+
+	//public Teacher search(String login,String password)
+		//throws Exception {
+        //Teacher teacher=null;
+
+		//Connection con=getConnection();
+
+		//PreparedStatement st;
+		//st=con.prepareStatement(
+			//"select * from teacher where login=? and password=?");
+		//st.setString(1, login);
+		//st.setString(2, password);
+		//ResultSet rs=st.executeQuery();
+
+		//while (rs.next()) {
+			//teacher=new Teacher();
+			//teacher.setId(rs.getInt("id"));
+			//teacher.setPassword(rs.getInt("password"));
+			//teacher.setName(rs.getString("name"));
+			//teacher.setSchool_cd(rs.getInt("school_cd"));
+		//}
+
+		//st.close();
+		//con.close();
+        //return teacher;
+	//}
 //}
