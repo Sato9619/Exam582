a615a57833172ff992352cb633c6d5425372c597
24coco1120na <knz2374431@stu.o-hara.ac.jp>
2024-04-30 10:36:45 +0900

    bean作成

---
+++ src/bean/Subject.java
@@ -0,0 +1,50 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Subject implements Serializable {
+
+	/**
+	 * 科目コード:String
+	 */
+	private String subject_cd;
+
+	/**
+	 * 科目名:String
+	 */
+	private String subject_name;
+
+	/**
+	 * 所属校:School
+	 */
+	private School school;
+
+
+
+	public String getSubject_cd() {
+		return subject_cd;
+	}
+
+	public void setSubject_cd(String subject_cd) {
+		this.subject_cd = subject_cd;
+	}
+
+	public String getSubject_name() {
+		return subject_name;
+	}
+
+	public void setSubject_name(String subject_name) {
+		this.subject_name = subject_name;
+	}
+
+	public School getSchool() {
+		return school;
+	}
+
+	public void setSchool(School school) {
+		this.school = school;
+	}
+
+
+
+}
